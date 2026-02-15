bc6520bc7444d00ac30188f164b47aa72f9c5e62
atoninai <usasaim111@gmail.com>
2026-02-15 17:31:13 +0600

    good

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,8 +21,8 @@ subprojects {
     // Force consistent JVM target across all subprojects (fixes on_audio_query_android mismatch)
     afterEvaluate {
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            kotlinOptions {
-                jvmTarget = "17"
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
             }
         }
         tasks.withType<JavaCompile>().configureEach {
